a3e67ea5def3e430526a57db7ab7997415242c8d
shibha <shibha20@gmail.com>
2013-01-09 18:27:27 +0530

    #374|+Sanchit|Added code for specific cell validation

--- GridSpike/src/main/webapp/grid-module.js
+++ GridSpike/src/main/webapp/grid-module.js
@@ -1,18 +1,18 @@
-var grid = angular.module('grid',['ngGrid']) ;
+var grid = angular.module('grid', ['ngGrid']);
 
 var INTEGER_REGEXP = /^\-?\d*$/;
 
-grid.directive('integer',function() {
+grid.directive('validator', function () {
     return {
         require:'ngModel',
-        link:function(scope,element,attrs,ctrl){
+        link:function (scope, element, attrs, ctrl) {
+            var validationFunction = scope.editableColumns[scope.$eval(attrs.validator)];
+            var validator = (validationFunction == undefined || validationFunction == "") ? null : grid[validationFunction];
             ctrl.$parsers.unshift(function (viewValue) {
-                if (INTEGER_REGEXP.test(viewValue)) {
-                    ctrl.$setValidity('integer', true);
+                if (validator == null || validator(viewValue, element.attr('name'))) {
                     document.getElementById(element.attr('name')).style.display = 'none';
-                     return viewValue;
+                    return viewValue;
                 } else {
-                    ctrl.$setValidity('integer', false);
                     document.getElementById(element.attr('name')).style.display = 'block';
                     return undefined;
                 }
@@ -20,4 +20,24 @@ grid.directive('integer',function() {
         }
     }
 });
+
+grid.integer = function (value, errorHolder) {
+    var toggleErrorMessageDisplay = function (valid, errorHolder) {
+        if (valid) {
+            document.getElementById(errorHolder).style.display = 'none';
+        } else {
+            document.getElementById(errorHolder).style.display = 'block';
+        }
+    };
+
+    var INTEGER_REGEXP = /^\d*$/;
+    var valid = INTEGER_REGEXP.test(value);
+
+    if (errorHolder != undefined) toggleErrorMessageDisplay(valid, errorHolder)
+
+    return valid;
+};
+
+
+
 
--- GridSpike/src/main/webapp/js/main.js
+++ GridSpike/src/main/webapp/js/main.js
@@ -1,44 +1,54 @@
 function bodyController($scope) {
     $scope.myData = [
-        {name:"Tiancum", age:43,address:"sh"},
-        {name:"Jacob", age:27,address:"sh"},
-        {name:"Nephi", age:29,address:"sh"},
-        {name:"Enos", age:34,address:"sh"},
-        {name:"Nephi1", age:29,address:"sh"},
-        {name:"Enos2", age:34,address:"sh"},
-        {name:"Nephi1sdsd", age:29,address:"sh"},
-        {name:"Enos1", age:34,address:"sh"}
+        {name:"Tiancum", age:43, address:"sh"},
+        {name:"Jacob", age:27, address:"sh"},
+        {name:"Nephi", age:29, address:"sh"},
+        {name:"Enos", age:34, address:"sh"},
+        {name:"Nephi1", age:29, address:"sh"},
+        {name:"Enos2", age:34, address:"sh"},
+        {name:"Nephi1sdsd", age:29, address:"sh"},
+        {name:"Enos1", age:34, address:"sh"}
     ];
-    $scope.gridOptions = { data:'myData' ,
+    $scope.gridOptions = { data:'myData',
         columnDefs:[
             { field:'name', displayName:'Full Name' },
-           { field:'age', displayName:'Age'}, //cellTemplate:editCell('row.entity.age')} ,
+            { field:'age', displayName:'Age'},//cellTemplate:editableCellTemplate('row.entity.age')} ,
             {field:'address', displayName:"Address"}
         ],
-       rowTemplate :  editRow()
+        rowTemplate:editableRowTemplate()
     };
 
-    function editCell(model) {
+    function editableCellTemplate(model) {
         var divElement = '<div ng-click="editable = true" ng-hide="editable">{{' + model + '}}</div>' +
             '<div><input ng-show="editable" ng-model="' + model + '"/>' +
             '<button ng-click="editable=false" ng-show="editable">ok</button></div>';
         return divElement;
     };
 
-    function editRow(){
-        var divElement = '<div ng-repeat="col in visibleColumns()" class="ngCell col{{$index}} {{col.cellClass}}" ng-cell><input  name={{$parent.$index}}{{$index}} integer ng-show="edit && isEditableColumn(col.field)" ng-model="row.entity[col.field]" /><span id={{$parent.$index}}{{$index}} style="display:none">Enter Numeric data!</span></div>'+
-            '<div class="editBtn"><button ng-click="edit=true" ng-hide="edit">edit</button>' +
-            '<button  ng-click="edit=false" ng-show="edit">ok</button></div>';
+    function editableRowTemplate() {
+        var divElement = '<div ng-repeat="col in visibleColumns()" class="ngCell col{{$index}} {{col.cellClass}}" ng-cell>'+
+            '<input name={{$parent.$index}}{{$index}} validator="col.field" ng-show="edit && isEditableColumn(col.field)" ng-model="row.entity[col.field]" />' +
+            '<span id={{$parent.$index}}{{$index}} style="display:none">Enter Numeric data!</span>' +
+            '</div>' +
+            '<div class="editBtn">' +
+            '<button ng-click="edit=true" ng-hide="edit">edit</button>' +
+            '<button  ng-click="edit=false" ng-show="edit">ok</button>' +
+            '</div>';
 
         return divElement;
     };
 
-    $scope.editableColumns = [
-        'age', 'address'
-    ];
+    $scope.editableColumns = {
+        age:'integer',
+        address:''
+    };
 
-    $scope.isEditableColumn = function(field){
-        return ($scope.editableColumns.indexOf(field)>=0);
-    } ;
+    $scope.isEditableColumn = function (field) {
+        for (var key in $scope.editableColumns) {
+            if (key == field) return true;
+        }
+        return false;
+    };
+
 
 }
