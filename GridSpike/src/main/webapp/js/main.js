3b6d29aa63fff9745c4ea509b35608ed61bd3c90
shibha <shibha20@gmail.com>
2013-01-09 15:18:41 +0530

    #374|+Sanchit | Added code for selective column inline edit

--- GridSpike/src/main/webapp/js/main.js
+++ GridSpike/src/main/webapp/js/main.js
@@ -9,7 +9,7 @@ function bodyController($scope) {
         {name:"Nephi1sdsd", age:29,address:"sh"},
         {name:"Enos1", age:34,address:"sh"}
     ];
-    $scope.gridOptions = { data:'myData',
+    $scope.gridOptions = { data:'myData' ,
         columnDefs:[
             { field:'name', displayName:'Full Name' },
            { field:'age', displayName:'Age'}, //cellTemplate:editCell('row.entity.age')} ,
@@ -26,17 +26,19 @@ function bodyController($scope) {
     };
 
     function editRow(){
-        var divElement = '<div ng-repeat="col in visibleColumns()" class="ngCell col{{$index}} {{col.cellClass}}"   ng-cell><input  name={{$parent.$index}}{{$index}} integer ng-show="edit" ng-model="row.entity[col.field]" /><span id={{$parent.$index}}{{$index}} style="display:none">Enter Numeric data!</span></div>'+
+        var divElement = '<div ng-repeat="col in visibleColumns()" class="ngCell col{{$index}} {{col.cellClass}}" ng-cell><input  name={{$parent.$index}}{{$index}} integer ng-show="edit && isEditableColumn(col.field)" ng-model="row.entity[col.field]" /><span id={{$parent.$index}}{{$index}} style="display:none">Enter Numeric data!</span></div>'+
             '<div class="editBtn"><button ng-click="edit=true" ng-hide="edit">edit</button>' +
             '<button  ng-click="edit=false" ng-show="edit">ok</button></div>';
 
         return divElement;
     };
 
-    $scope.showError=function(){
-       var temp = "row.entity[col.field]";
-        return  'form.'+ temp+'.$error.validInteger';
-    }
+    $scope.editableColumns = [
+        'age', 'address'
+    ];
 
+    $scope.isEditableColumn = function(field){
+        return ($scope.editableColumns.indexOf(field)>=0);
+    } ;
 
 }
