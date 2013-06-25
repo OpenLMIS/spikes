c1b0b6366916febdb25f68c9298d9a8af3986373
shibha <shibhama@thoughtworks.com>
2013-06-25 14:59:24 +0530

    |#678|+Manan|Spiked on edit , update , delete and handing error scenarios for indexeddb

--- IndexedDbSpike/js/main.js
+++ IndexedDbSpike/js/main.js
@@ -1,35 +1,75 @@
 function FormController($scope) {
 
-  var db;
-  var request = indexedDB.open("MyDb", 2);
+    var db;
+    var request = indexedDB.open("MyDb", 3);
+    var transaction;
 
-  request.onsuccess = function (event) {
-    db = request.result;
-  };
+    $scope.refreshData = function () {
+        var objectStore = db.transaction('objects').objectStore('objects');
+        $scope.dataArr = [];
 
-  request.onerror = function (e) {
-    console.error(e, request);
-  };
+        objectStore.openCursor().onsuccess = function (event) {
+            var cursor = event.target.result;
+            if (cursor) {
+                $scope.dataArr.push(cursor.value);
+                cursor.continue();
+            }
+            $scope.$apply();
 
-  request.onupgradeneeded = function (evt) {
-    db = request.result;
-    var formsStore = db.createObjectStore("objects", {
-      "keyPath": "id",
-      "autoIncrement": true
-    });
-    formsStore.createIndex("by_code", "code", { unique: true});
-    formsStore.createIndex("by_name", "name", { unique: true });
-    console.info(db);
-  };
+        };
+    };
 
-  $scope.save = function (data) {
-    var transaction = db.transaction(['objects'], 'readwrite');
-    var objects = transaction.objectStore('objects');
+    request.onsuccess = function (event) {
+        db = request.result;
+        $scope.refreshData();
+    };
 
-    objects.put(data);
+    request.onerror = function (e) {
+        console.error(e, request);
+    };
 
-    transaction.oncomplete = function() {
-      console.log('done');
+    request.onupgradeneeded = function (evt) {
+        db = request.result;
+        var formsStore = db.createObjectStore("objects", {
+            "keyPath": "id",
+            "autoIncrement": true
+        });
+        formsStore.createIndex("by_code", "code", { unique: true});
+        formsStore.createIndex("by_name", "name", { unique: true });
+        console.info(db);
+    };
+
+
+    $scope.save = function (data) {
+        var isUpdate = false;
+        if (data.id) isUpdate = true;
+        transaction = db.transaction('objects', 'readwrite');
+        var objects = transaction.objectStore('objects');
+
+        objects.put(data);
+
+        transaction.oncomplete = function () {
+            if (isUpdate) console.log('updated');
+            else console.log('created');
+
+            $scope.refreshData();
+        }
+        transaction.onabort = function() {
+            console.log(transaction.error);
+        }
     }
-  }
+
+
+    $scope.delete = function (data) {
+        transaction = db.transaction('objects', 'readwrite');
+        var objects = transaction.objectStore('objects');
+
+        objects.delete(data.id);
+
+        transaction.oncomplete = function () {
+            console.log('deleted');
+            $scope.refreshData();
+        }
+    }
+
 }
